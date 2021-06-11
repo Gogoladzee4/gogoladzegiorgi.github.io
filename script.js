4f064e256de4778338101a6f533268fb042e18df
Gogoladzee4 <69477346+Gogoladzee4@users.noreply.github.com>
2021-06-11 01:15:08 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let ima = document.getElementById('prlx');
+let ima2 = document.getElementById('prlx2');
+let ima1 = document.getElementById('pxl');
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+
+    ima.style.right =   value * 2 + 'px';
+    ima2.style.bottom =  -50 + value * 0.5 +'px';
+   // ima1.style.bottom = 90 + value * 2 + 'px';
+})
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.up').addClass("scrol");
+    
+        }else{
+            $('.up').removeClass("scrol");
+        }
+    });
